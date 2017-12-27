a475c8c546be21ed0c39b5d7b5ee58a47323a1c7
Will <157747074@qq.com>
2017-12-27 17:37:45 +0800

    add comment

--- fab_script/install_shadowsocks/config_template.py
+++ fab_script/install_shadowsocks/config_template.py
@@ -1,13 +1,18 @@
 # coding = utf8
 
+# the hosts you want to install ss and privoxy
 HOSTS = [
     "apple@8.8.8.8:4444"
     # "qiuweilue@219.135.102.83:9529"
 ]
 
+# hosts password
 PWD = 'appleisgood'
 
+# ss bind port
 shadowsocks_local_port = 1086
+
+# ss config
 shadowsocks_config = {
     "server":"google.com",
     "server_port":8089,
@@ -17,6 +22,11 @@ shadowsocks_config = {
     "method":"aes-256-cfb"
 }
 
+# some download will put in this dir
 home_dir = "/home/apple"
+
+# privoxy install path
 privoxy_install_path = '/usr/local/privoxy'
+
+# privoxy bind port
 privoxy_port = 8118
